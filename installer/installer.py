2c5fa5f7ddfce86e495187b316fd8c8a7b362f37
Jure <grcar.jure1@gmail.com>
2023-11-13 21:01:19 +0100

    updated installer code

--- installer/installer.py
+++ installer/installer.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import requests
 import sys
+import winreg
+import ctypes
 
 #############################################################################
 # VARIABLES                                                                 #
@@ -56,7 +58,6 @@ def browse_path():
     global installation_path
     installation_path = r'{}'.format(path)
 
-    
 
 def add_to_path(path):
     # Check if the path already exists in the PATH environment variable
@@ -67,9 +68,27 @@ def add_to_path(path):
         # Save the updated PATH environment variable
         cmd = 'setx PATH "{}"'.format(os.environ['PATH'])
         subprocess.call(cmd, shell=True)
+
+def add_to_system_path(path):
+    # Open the registry key
+    with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SYSTEM\CurrentControlSet\Control\Session Manager\Environment', 0, winreg.KEY_ALL_ACCESS) as key:
+        # Get the current PATH
+        current_path = winreg.QueryValueEx(key, 'Path')[0]
+
+        # Add the new path to the current PATH
+        new_path = current_path + ';' + path
+
+        # Set the new PATH
+        winreg.SetValueEx(key, 'Path', 0, winreg.REG_EXPAND_SZ, new_path)
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
     
 def install_pangolang():
-    pangobat_path = installation_path + "/pangolang_v1.2/pango.bat"
+    pangobat_path = installation_path + "/pango.bat"
     # get pangolang files from github
     response = requests.get(api_url, headers={})
 
@@ -96,7 +115,8 @@ def install_pangolang():
         sys.exit()
 
     # add pango.bat to the path
-    add_to_path(pangobat_path)
+    add_to_system_path(pangobat_path)
+    sys.exit()
 
 #                                                                           #
 #############################################################################
@@ -106,27 +126,30 @@ def install_pangolang():
 root = tk.Tk()
 root.title("Pangolang installer")
 
-
 # Create a label for the combined text (ASCII art and additional text)
 combined_label = tk.Label(root, text=pangolang_text + pangolang_logo, font=("Courier New", 10))
 combined_label.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
 
-# Create a label for the path entry
-entry_label = tk.Label(root, text="Enter a folder to install Pangolang files in:")
-entry_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+if is_admin():
+    # Create a label for the path entry
+    entry_label = tk.Label(root, text="Enter a folder to install Pangolang files in:")
+    entry_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-# Create an entry widget for path input
-entry_path = tk.Entry(root, width=50)
-entry_path.grid(row=1, column=1, padx=10, pady=5)
+    # Create an entry widget for path input
+    entry_path = tk.Entry(root, width=50)
+    entry_path.grid(row=1, column=1, padx=10, pady=5)
 
-# Create a "Browse" button
-browse_button = tk.Button(root, text="Browse", command=browse_path)
-browse_button.grid(row=1, column=2, padx=10, pady=5)
+    # Create a "Browse" button
+    browse_button = tk.Button(root, text="Browse", command=browse_path)
+    browse_button.grid(row=1, column=2, padx=10, pady=5)
 
-# Create an "Install" button
-install_button = tk.Button(root, text="Install", command=install_pangolang)
-install_button.config(height=2, width=20)
-install_button.grid(row=2, column=0, columnspan=3, pady=20)
+    # Create an "Install" button
+    install_button = tk.Button(root, text="Install", command=install_pangolang)
+    install_button.config(height=2, width=20)
+    install_button.grid(row=2, column=0, columnspan=3, pady=20)
+else:
+    admin_label = tk.Label(root, text="RUN THE INSTALLER AS AN ADMINISTRATOR", font=("Courier New", 20))
+    admin_label.grid(row=1, column=0, padx=10, pady=10, columnspan=3)
 
 root.mainloop()
 #                                                                           #
